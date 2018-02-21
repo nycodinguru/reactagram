8aff96f3cf6cc44cf49c4fa43484548465c1febb
Rashad <rashadd.rose@gmail.com>
2018-02-20 22:36:19 -0500

    slight css fixes

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -9,13 +9,16 @@ export default class NavBar extends Component {
       
     }
 
-		return(
+		return( 
+        <div className="nav-shadow">
             <nav className="clearfix"> 
               <a href="/" id="react-logo"> </a>
               <img src="https://i.imgur.com/ibE1DjA.png" id="reactagram-logo" alt="https://i.imgur.com/ibE1DjA.png"/>
               {/*<div id="reactagram-logo"></div>*/}
               <a href="/users/:id" id="profile-photo"> </a>
             </nav>
+        </div>
 		)
 	}
 }
+
